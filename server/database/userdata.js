604e8b6c2d9d1bd45f7e65e895363a1e9197066c
AtharvaThorve <aaathorve@gmail.com>
2020-10-27 22:48:10 +0530

    Dummy Userdata added

---
+++ server/database/userdata.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+const driver = require('./config');
+
+const filePath = path.join(__dirname, 'userdata.json');
+
+const addData = async () => {
+	fs.readFile(filePath, { encoding: 'utf-8' }, async (err, data) => {
+		if (err) {
+			console.log(err);
+		}
+		let session;
+		try {
+			session = driver.session();
+			await session.run('MATCH (a:User) DELETE a');
+			await session.close();
+		} catch (error) {
+			console.log(error);
+		}
+		const userData = JSON.parse(data);
+		userData.forEach(async d => {
+			session = driver.session();
+			try {
+				await session.run(
+					'CREATE (a:User {firstName: $first_name, lastName: $last_name, email: $email, id: $id, lat: $lat, long: $long, expiry_of_plan: $eop, amount_of_usage: $aou}) RETURN a',
+					{
+						first_name: d.first_name,
+						last_name: d.last_name,
+						email: d.email,
+						id: d.id.$oid,
+						lat: d.latitude,
+						long: d.longitude,
+						eop: d.plan_of_expiry,
+						aou: d.amount_of_usage,
+					}
+				);
+			} catch (error1) {
+				console.log(error1);
+			}
+			await session.close();
+		});
+	});
+	await driver.close();
+};
+
+addData();

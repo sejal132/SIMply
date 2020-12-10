c755dd41dd7612ccd02e986ffce1a7ba5e30beff
AtharvaThorve <aaathorve@gmail.com>
2020-12-10 11:16:11 +0530

    Signup form completed

--- client/src/components/Form/SignupForm.jsx
+++ client/src/components/Form/SignupForm.jsx
@@ -1,7 +1,206 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Spinner from '../Spinner/Spinner';
 import './SignupForm.css';
 
 const SignupForm = props => {
+	const [firstName, setFirstName] = useState('');
+	const [lastName, setLastName] = useState('');
+	const [email, setEmail] = useState('');
+	const [costPerMonth, setCostPerMonth] = useState();
+	const [planExpiry, setPlanExpiry] = useState(new Date().toISOString());
+	const [lat, setLat] = useState(0);
+	const [long, setLong] = useState(0);
+	const [amountPerDay, setAmountPerDay] = useState();
+	const [provider, setProvider] = useState('');
+	const [type, setType] = useState('');
+	const [profession, setProfession] = useState('');
+	const [country, setCountry] = useState('');
+	const [rating, setRating] = useState();
+	const [isLoading, setIsLoading] = useState(false);
+
+	const [errors, setErrors] = useState({
+		firstName: '',
+		lastName: '',
+		email: '',
+		costPerMonth: '',
+		rating: '',
+		amountPerDay: '',
+		provider: '',
+		type: '',
+		profession: '',
+		country: '',
+	});
+
+	const handleFirstNameChange = e => setFirstName(e.target.value);
+
+	const handleLastNameChange = e => setLastName(e.target.value);
+
+	const handleEmailChange = e => setEmail(e.target.value);
+
+	const handleCostChange = e => {
+		if (e.target.value > 401) {
+			setCostPerMonth(401);
+		} else {
+			setCostPerMonth(e.target.value);
+		}
+	};
+
+	const handleDateChange = e => setPlanExpiry(e.target.value);
+
+	const handleProviderChange = e => setProvider(e.target.value);
+
+	const handleAmountChange = e => setAmountPerDay(e.target.value);
+
+	const handleTypeChange = e => setType(e.target.value);
+
+	const handleProfessionChange = e => setProfession(e.target.value);
+
+	const handleCountryChange = e => setCountry(e.target.value);
+
+	const handleRatingChange = e => setRating(e.target.value);
+
+	const isFormValid = () => {
+		let formIsValid = true;
+		if (!firstName) {
+			formIsValid = false;
+			setErrors(prevErrors => ({
+				...prevErrors,
+				firstName: '*First Name cannot be empty',
+			}));
+		}
+		if (!lastName) {
+			formIsValid = false;
+			setErrors(prevErrors => ({
+				...prevErrors,
+				lastName: '*Last Name cannot be empty',
+			}));
+		}
+		if (!email) {
+			formIsValid = false;
+			setErrors(prevErrors => ({
+				...prevErrors,
+				email: "*Email can't be Empty",
+			}));
+		} else {
+			let pattern = new RegExp(
+				/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+			);
+			if (!pattern.test(email)) {
+				formIsValid = false;
+				setErrors(prevErrors => ({
+					...prevErrors,
+					errors: '*Please enter valid Email ID',
+				}));
+			}
+		}
+		if (costPerMonth === 0 || !costPerMonth) {
+			formIsValid = false;
+			setErrors(prevErrors => ({
+				...prevErrors,
+				costPerMonth: '*Cost per month cannot be zero or empty',
+			}));
+		}
+		if (!amountPerDay) {
+			formIsValid = false;
+			setErrors(prevErrors => ({
+				...prevErrors,
+				amountPerDay: '*Please select amount per day',
+			}));
+		}
+		if (provider.length === 0) {
+			formIsValid = false;
+			setErrors(prevErrors => ({
+				...prevErrors,
+				provider: '*Please select a provider',
+			}));
+		}
+		if (type.length === 0) {
+			formIsValid = false;
+			setErrors(prevErrors => ({
+				...prevErrors,
+				type: '*Please select type of plan',
+			}));
+		}
+		if (profession.length === 0) {
+			formIsValid = false;
+			setErrors(prevErrors => ({
+				...prevErrors,
+				profession: '*Please select your profession',
+			}));
+		}
+		if (country.length === 0) {
+			formIsValid = false;
+			setErrors(prevErrors => ({
+				...prevErrors,
+				country: '*Please select country',
+			}));
+		}
+		if (!rating) {
+			formIsValid = false;
+			setErrors(prevErrors => ({
+				...prevErrors,
+				rating: '*Rating cannot empty',
+			}));
+		}
+		return formIsValid;
+	};
+
+	useEffect(() => {
+		navigator.geolocation.getCurrentPosition(position => {
+			setLat(position.coords.latitude);
+			setLong(position.coords.longitude);
+		});
+	}, []);
+
+	const formSubmitHandler = async e => {
+		if (isFormValid()) {
+			const dataObj = {
+				firstName: firstName,
+				lastName: lastName,
+				email: email,
+				planExpiry: planExpiry,
+				lat: lat,
+				long: long,
+				amountPerDay: parseFloat(amountPerDay),
+				provider: provider,
+				costPerMonth: parseFloat(costPerMonth),
+				type: type,
+				profession: profession,
+				country: country,
+				userRating: rating,
+			};
+			console.log(dataObj);
+			setErrors({
+				firstName: '',
+				lastName: '',
+				email: '',
+				costPerMonth: '',
+				rating: '',
+				amountPerDay: '',
+				provider: '',
+				type: '',
+				profession: '',
+				country: '',
+			});
+			try {
+				setIsLoading(true);
+				const result = await axios.post(
+					'http://localhost:8080/adduser',
+					dataObj
+				);
+				console.log(result.data);
+				localStorage.setItem('id', result.data);
+				props.history.push('/recommend');
+			} catch (error) {
+				console.log(error);
+			}
+			setIsLoading(false);
+		} else {
+			e.preventDefault();
+		}
+	};
+
 	return (
 		<div class='container register'>
 			<div class='row'>
@@ -28,7 +227,8 @@ const SignupForm = props => {
 											type='text'
 											class='form-control'
 											placeholder='First Name *'
-											value=''
+											value={firstName}
+											onChange={handleFirstNameChange}
 										/>
 									</div>
 									<div class='form-group'>
@@ -36,7 +236,8 @@ const SignupForm = props => {
 											type='text'
 											class='form-control'
 											placeholder='Last Name *'
-											value=''
+											value={lastName}
+											onChange={handleLastNameChange}
 										/>
 									</div>
 									<div class='form-group'>
@@ -44,7 +245,8 @@ const SignupForm = props => {
 											type='email'
 											class='form-control'
 											placeholder='Your Email *'
-											value=''
+											value={email}
+											onChange={handleEmailChange}
 										/>
 									</div>
 									<div class='form-group'>
@@ -54,7 +256,8 @@ const SignupForm = props => {
 											max='401'
 											class='form-control'
 											placeholder='Cost per month *'
-											value=''
+											value={costPerMonth}
+											onChange={handleCostChange}
 										/>
 									</div>
 									<div class='form-group'>
@@ -71,140 +274,117 @@ const SignupForm = props => {
 											}
 											class='form-control'
 											placeholder='Expiry of plan *'
-											value=''
+											value={planExpiry}
+											onChange={handleDateChange}
 										/>
 									</div>
 								</div>
 								<div class='col-md-6'>
 									<div class='form-group'>
-										<span>Service Provider* </span>
-										<select class='form-control'>
-											<option selected>
-												Jio
+										<select
+											class='form-control'
+											onChange={handleProviderChange}>
+											<option value='' selected disabled>
+												Select Provider *
 											</option>
-											<option>
+											<option value='Jio'>Jio</option>
+											<option value='Vodafone'>
 												Vodafone
 											</option>
-											<option>
-												Idea
-											</option>
-											<option>
+											<option value='Idea'>Idea</option>
+											<option value='Airtel'>
 												Airtel
 											</option>
-											<option>
-												BSNL
+											<option value='BSNL'>BSNL</option>
+										</select>
+									</div>
+									<div class='form-group'>
+										<select
+											class='form-control'
+											onChange={handleTypeChange}>
+											<option value='' selected disabled>
+												Select Type *
+											</option>
+											<option value='prepaid'>
+												Prepaid
+											</option>
+											<option value='postpaid'>
+												Postpaid
+											</option>
+										</select>
+									</div>
+									<div class='form-group'>
+										<select
+											class='form-control'
+											onChange={handleAmountChange}>
+											<option value='' selected disabled>
+												Select Amount of Data *
+											</option>
+											<option value={1}>1</option>
+											<option value={1.5}>1.5</option>
+											<option value={2}>2</option>
+											<option value={3}>3</option>
+										</select>
+									</div>
+									<div class='form-group'>
+										<select
+											class='form-control'
+											onChange={handleProfessionChange}>
+											<option value='' selected disabled>
+												Select Profession *
+											</option>
+											<option value='student'>
+												Student
+											</option>
+											<option value='travel'>
+												Travel
+											</option>
+											<option value='work'>Work</option>
+										</select>
+									</div>
+									<div class='form-group'>
+										<select
+											class='form-control'
+											onChange={handleCountryChange}>
+											<option value='' selected disabled>
+												Select Country *
+											</option>
+											<option value='India'>India</option>
+											<option value='USA'>USA</option>
+											<option value='Great Britain'>
+												Great Britain
+											</option>
+											<option value='Australia'>
+												Australia
+											</option>
+											<option value='Russia'>
+												Russia
 											</option>
 										</select>
 									</div>
 									<div class='form-group'>
 										<input
-											type='text'
+											type='number'
 											class='form-control'
-											placeholder='Enter Your Answer *'
-											value=''
+											placeholder='Rating *'
+											value={rating}
+											onChange={handleRatingChange}
+											min='1'
+											max='10'
 										/>
 									</div>
-									<input
-										type='submit'
-										class='btnRegister'
-										value='Register'
-									/>
-								</div>
-							</div>
-						</div>
-						<div
-							class='tab-pane fade show'
-							id='profile'
-							role='tabpanel'
-							aria-labelledby='profile-tab'>
-							<h3 class='register-heading'>Apply as a Hirer</h3>
-							<div class='row register-form'>
-								<div class='col-md-6'>
-									<div class='form-group'>
-										<input
-											type='text'
-											class='form-control'
-											placeholder='First Name *'
-											value=''
-										/>
-									</div>
-									<div class='form-group'>
-										<input
-											type='text'
-											class='form-control'
-											placeholder='Last Name *'
-											value=''
-										/>
-									</div>
-									<div class='form-group'>
-										<input
-											type='email'
-											class='form-control'
-											placeholder='Email *'
-											value=''
-										/>
-									</div>
-									<div class='form-group'>
-										<input
-											type='text'
-											maxlength='10'
-											minlength='10'
-											class='form-control'
-											placeholder='Phone *'
-											value=''
-										/>
-									</div>
-								</div>
-								<div class='col-md-6'>
-									<div class='form-group'>
-										<input
-											type='password'
-											class='form-control'
-											placeholder='Password *'
-											value=''
-										/>
-									</div>
-									<div class='form-group'>
-										<input
-											type='password'
-											class='form-control'
-											placeholder='Confirm Password *'
-											value=''
-										/>
-									</div>
-									<div class='form-group'>
-										<select class='form-control'>
-											<option
-												class='hidden'
-												selected
-												disabled>
-												Please select your Sequrity
-												Question
-											</option>
-											<option>
-												What is your Birthdate?
-											</option>
-											<option>
-												What is Your old Phone Number
-											</option>
-											<option>
-												What is your Pet Name?
-											</option>
-										</select>
-									</div>
-									<div class='form-group'>
-										<input
-											type='text'
-											class='form-control'
-											placeholder='`Answer *'
-											value=''
-										/>
-									</div>
-									<input
-										type='submit'
-										class='btnRegister'
-										value='Register'
-									/>
+									{isLoading ? (
+										<div class='spinner'>
+											<Spinner />
+										</div>
+									) : (
+										<button
+											type='button'
+											class='btnRegister'
+											onClick={formSubmitHandler}>
+											SignUp
+										</button>
+									)}
 								</div>
 							</div>
 						</div>
